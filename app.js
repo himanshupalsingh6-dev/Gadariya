ee15ed3fc54c676565d482bd2a7480bd7780d743
Himanshu pal <himanshupalsingh6@gmail.com>
2026-04-01 21:31:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-szdgfhgjk
+let products = [
+  {name:"Shirt", price:15, qty:0},
+  {name:"Pant", price:20, qty:0},
+  {name:"Saree", price:30, qty:0}
+];
+
+function loadProducts(){
+  let html="";
+  products.forEach((p,i)=>{
+    html += `
+      <div class="qty">
+        ${p.name} ₹${p.price}
+        <button onclick="add(${i})">+</button>
+      </div>
+    `;
+  });
+  document.getElementById("productList").innerHTML = html;
+}
+
+function add(i){
+  products[i].qty++;
+  localStorage.setItem("cart", JSON.stringify(products));
+  alert("Added to cart");
+} 
