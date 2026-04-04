b7fa422fbe8afa0ec223d36afebfa5c897a9d132
Himanshu pal <himanshupalsingh6@gmail.com>
2026-04-04 13:05:34 +0530

    x

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 let products = [
-  {name:"Shirt", price:15, qty:0},
-  {name:"Pant", price:20, qty:0},
-  {name:"Saree", price:30, qty:0}
+  {name:"Shirt Iron", price:15, qty:0},
+  {name:"Pant Iron", price:20, qty:0},
+  {name:"Saree Iron", price:30, qty:0}
 ];
 
 function loadProducts(){
   let html="";
+
   products.forEach((p,i)=>{
     html += `
-      <div class="qty">
-        ${p.name} ₹${p.price}
-        <button onclick="add(${i})">+</button>
+      <div class="card">
+        <h4>${p.name}</h4>
+        <p>₹${p.price}</p>
+        <button onclick="addToCart(${i})">Add</button>
       </div>
     `;
   });
+
   document.getElementById("productList").innerHTML = html;
 }
 
-function add(i){
+function addToCart(i){
   products[i].qty++;
   localStorage.setItem("cart", JSON.stringify(products));
   alert("Added to cart");
-} 
+}
